4f14a38dec8fee0ba148fb14e4ae704ff637feb9
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-20 21:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeColor() {
+	const SelectEl=document.getElementById('colorSelect');
+	const selectCl=SelectEl.value;
+	const options= SelectEl.options;
+	for (let i = 0; i < options.length; i++) {
+		if(options[i].value===selectCl){
+			SelectEl.removeChild(options[i]);
+			break;
+		}
+	}
+}
